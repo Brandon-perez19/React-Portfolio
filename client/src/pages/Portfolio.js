22f44813131912895623c737fcf778235616f199
Brandon <brandonperez@brandons-mbp.lan>
2022-07-27 20:48:52 -0400

    added list, github links to portfolio aspect, downloadable resume.

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import budgetTracker from '../assets/Budget-Tracker.png'
 import teamProfileGenerator from '../assets/Team-Profile-Generator.png'
 import eCommerce from '../assets/E-Commerce.png'
@@ -11,32 +11,38 @@ function Portfolio() {
         {
             name: "Book Search Engine",
             image: bookSearch,
-            link: "https://salty-mountain-82785.herokuapp.com/"
+            link: "https://salty-mountain-82785.herokuapp.com/",
+            github:"https://github.com/Brandon-perez19/Book-Search-Engine"
         },
         {
             name: "Budget Tracker",
             image: budgetTracker,
-            link: "https://aqueous-castle-91439.herokuapp.com/"
+            link: "https://aqueous-castle-91439.herokuapp.com/",
+            github:"https://github.com/Brandon-perez19/Budget-Tracker"
         },
         {
             name: "Deep Thoughts",
             image: deepThoughts,
-            link: "https://hidden-badlands-71187.herokuapp.com/"
+            link: "https://hidden-badlands-71187.herokuapp.com/",
+            github: "https://github.com/Brandon-perez19/deep-thoughts"
         },
         {
             name: "E-Commerce",
             image: eCommerce,
-            link: "https://github.com/Brandon-perez19/E-Commerce"
+            link: "https://github.com/Brandon-perez19/E-Commerce",
+            github: "https://github.com/Brandon-perez19/E-Commerce"
         },
         {
             name: "Team Profile Generator",
             image: teamProfileGenerator,
-            link: "https://github.com/Brandon-perez19/Team-Profile-Generator"
+            link: "https://github.com/Brandon-perez19/Team-Profile-Generator",
+            github: "https://github.com/Brandon-perez19/Team-Profile-Generator"
         },
         {
             name: "Tech Blog",
             image: techBlog,
-            link: "https://whispering-plateau-17922.herokuapp.com/"
+            link: "https://whispering-plateau-17922.herokuapp.com/",
+            github:"https://github.com/Brandon-perez19/Tech-Blog"
         }
     ]
     return (
@@ -47,12 +53,13 @@ function Portfolio() {
                     <div className="col-5 my-2">
                         <div className="border rounded border-dark">
                             <div className="card">
-                                <a href={project.link} target="_blank">
+                                <a href={project.link} rel="noreferrer" target="_blank">
                                 <img src={project.image} className="card-img-top" alt={project.name} />
                                 </a>
                                 <div className="card-body">
                                     <h5 className="card-title">{project.name}</h5>
                                     <p className="card-text">{project.description}</p>
+                                    <a href={project.github} rel="noreferrer" target="_blank">GitHub Repository</a>
                                 </div>
                             </div>
                         </div>
--- client/src/pages/Resume.js
+++ client/src/pages/Resume.js
@@ -1,7 +1,30 @@
-function Resume () {
-    return(
+import resume from '../assets/resume.jpeg'
+
+function Resume() {
+    return (
+
         <div>
-            I'm the resume
+            <div >
+                <h2 className='text-muted'> Professional Skills </h2>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item"> Database integration</li>
+                    <li className="list-group-item"> MongoDB</li>
+                    <li className="list-group-item"> SQL</li>
+                    <li className="list-group-item"> Node.js</li>
+                    <li className="list-group-item"> Express.js</li>
+                    <li className="list-group-item"> Html, CSS </li>
+                    <li className="list-group-item"> JavaScript</li>
+                    <li className="list-group-item"> BootStrap</li>
+                    <li className="list-group-item"> RESTful API</li>
+                    <li className="list-group-item"> React</li>
+                    <li className="list-group-item"> Analytical thinking</li>
+                    <li className="list-group-item"> Resolving technical issues</li>
+                    <li className="list-group-item"> Communication skills</li>
+                    <li className="list-group-item"> Teamwork </li>
+                    <li className="list-group-item"> Adaptability </li>
+                </ul>
+                <h4>Click <a href={resume} className="text-decoration-none" download> here </a> to download my resume! </h4>
+            </div>
         </div>
     )
 }
